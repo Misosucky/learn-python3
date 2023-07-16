094aff4cf5cede095d58118e1a6d37e64235bb06
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-07-16 09:48:25 -0400

    added notes for myself throughtout program

--- ex3.py
+++ ex3.py
@@ -1,23 +1,23 @@
 print("I will now count my chickens:")
-#: 
-print("Hens", 25 + 30 / 6)
-print("Roosters", 100 - 25 * 3 % 4 )
+# : in print function is stritcly a text
+print("Hens", 25 + 30 / 6) # is n + (30/6)
+print("Roosters", 100 - 25 * 3 % 4 ) # % is not percent it is a modulus; X/X R
 
 print("Now I will count my eggs:")
 
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) # Do not understand Modulus yet, so am unable to calculate ny hand
 
-print("Is it true that 3 + 2 < 5 - 7?")
+print("Is it true that 3 + 2 < 5 - 7?") # this appears to add a T/F output based on the follwoing line
 
 print(3 +2 < 5 - 7)
 
-print("What is 3+2?" , 3 + 2)
+print("What is 3+2?" , 3 + 2) # ,Creates a string which allows for 2 outputs
 print("What is 5 -7?" , 5 - 7)
 
 print("Oh, that's why it's False.")
 
 print("How about some more.")
 
-print("Is it greater?" , 5 > -2)
-print("Is it greater or equal?" , 5 >= -2)
-print("Is it less or equal?" , 5 <= -2)
+print("Is it greater?" , 5 > -2) # text is strictly for the reader, CPU uses math after ,
+print("Is it greater or equal?" , 5 >= -2) #^
+print("Is it less or equal?" , 5 <= -2) #^ ^ ^
