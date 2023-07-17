789babba44f39f88800b7307593d89a492bede21
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-07-17 18:22:23 -0400

    added lines 10-11

--- ex3v2.py
+++ ex3v2.py
@@ -6,3 +6,8 @@ print("Lets see how many hours you need to work to make $650 in one week, what i
 print("$21.20")
 print("Sweet! That means you need to work", 650/21.60 , "hours to make $650 a week")
 print ("At that rate you could bring home $" , 21.60 * 40 * 52, "per year" )
+print ("Too bad you have to pay bills...")
+print ("What is your total bill expense per month?")
+print (700 + 100 + 150 + 275 + 600 + 30 + 50)
+print("woah")
+print("That leaves you with" , 21.60 * 40 * 52 - 700 + 100 + 150 + 275 + 600 + 30 + 50 )
